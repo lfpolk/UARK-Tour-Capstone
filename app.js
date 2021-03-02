3848d227cc75d6b06099ff500d07bb5102467517
osanabria <you@example.com>
2021-03-02 01:48:15 -0600

    ssl

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
-//app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.use(require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
