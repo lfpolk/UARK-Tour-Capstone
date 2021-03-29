0a3d1353468a527c4501c171df05183bb35530e2
osanabria <you@example.com>
2021-03-28 22:04:19 -0500

    updating marker

--- public/logic.js
+++ public/logic.js
@@ -5,24 +5,24 @@ window.onload =  function() {
         }
         
         const map = new google.maps.Map(document.getElementById("map"), options);
+        const marker = new google.maps.Marker({
+            map,
+            title: "Hello World!",
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 10,
+                fillOpacity: 1,
+                strokeWeight: 2,
+                fillColor: '#5384ED',
+                strokeColor: '#ffffff'
+            }
+        }); 
     if(navigator.geolocation) {
         navigator.geolocation.watchPosition(function(position) {
             
                 user_location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
                 map.setCenter(user_location);
-                new google.maps.Marker({
-                    position: user_location,
-                    map,
-                    title: "Hello World!",
-                    icon: {
-                        path: google.maps.SymbolPath.CIRCLE,
-                        scale: 10,
-                        fillOpacity: 1,
-                        strokeWeight: 2,
-                        fillColor: '#5384ED',
-                        strokeColor: '#ffffff'
-                    }
-                });       
+                marker.setPosition(user_location);
             },
             function (error) {
             alert(error.message);
