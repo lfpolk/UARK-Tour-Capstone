735d6dc8e10bc7ccfad89969c3f131350c5168dc
osanabria <you@example.com>
2021-03-29 02:12:06 -0500

    adding directions to union

--- public/logic.js
+++ public/logic.js
@@ -1,11 +1,12 @@
 var marker;
+var map;
 
 window.onload =  function() { 
     var options ={
         zoom:15
     }
     
-    const map = new google.maps.Map(document.getElementById("map"), options);
+    map = new google.maps.Map(document.getElementById("map"), options);
     marker = new google.maps.Marker({
         map,
         title: "Hello World!",
@@ -18,9 +19,6 @@ window.onload =  function() {
             strokeColor: '#ffffff'
         }
     });
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    directionsDisplay.setMap(map);
     if(navigator.geolocation) {
         navigator.geolocation.watchPosition(function(position) {
                 var LatLng = { lat: position.coords.latitude, lng: position.coords.longitude }
@@ -28,17 +26,7 @@ window.onload =  function() {
                 map.setCenter(user_location);
                 marker.setPosition(user_location);
                 
-                directionsService.route({
-                    origin: LatLng,
-                    destination: "Arkansas Union, 435 Garland Ave, Fayetteville, AR 72701, USA",
-                    travelMode: 'WALKING'
-                }, function(response, status) {
-                    if (status === 'OK') {
-                    directionsDisplay.setDirections(response);
-                    } else {
-                    window.alert('Directions request failed due to ' + status);
-                    }
-                });
+                
             },
             function (error) {
             alert(error.message);
@@ -52,12 +40,33 @@ window.onload =  function() {
     }
 };
 
-
-function initMap() {
+function startTour()
+{
     var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    directionsDisplay.setMap(map);
-    
-}
-
-    
+    var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            directionsService.route({
+                origin: { lat: position.coords.latitude, lng: position.coords.longitude },
+                destination: "Arkansas Union, 435 Garland Ave, Fayetteville, AR 72701, USA",
+                travelMode: 'WALKING'
+            }, function(response, status) {
+                if (status === 'OK') {
+                directionsDisplay.setDirections(response);
+                } else {
+                window.alert('Directions request failed due to ' + status);
+                }
+            })
+            },
+            
+            function (error) {
+            alert(error.message);
+            }, {
+            enableHighAccuracy: true
+            , timeout: 5000
+            });
+        }
+        else {
+        document.getElementById('latitude').textContent = "FAIL";
+    }
+};    
